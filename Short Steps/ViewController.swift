622bd35ae087012280d70019b2263aa2ddb06424
Justin DeMaris <justin@getsynced.co>
2015-02-16 10:47:36 -0500

    Initial functioning version of Short Steps with lots of dog pics and iOS 8+ min requirement

--- Short Steps/ViewController.swift
+++ Short Steps/ViewController.swift
@@ -7,19 +7,79 @@
 //
 
 import UIKit
+import CoreMotion
+import Foundation
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var stepCount: UILabel!
 
+    let pedometer = CMPedometer()
+    
+    let lengthFormatter = NSLengthFormatter()
+    
+    let dateFormatter = NSDateFormatter()
+    
+    let imageIndex = [
+        UIImage(named:"DSC06971.jpg"),
+        UIImage(named:"IMG_0264.jpg"),
+        UIImage(named:"IMG_0345.jpg"),
+        UIImage(named:"IMG_0354.jpg"),
+        UIImage(named:"IMG_0381.jpg"),
+        UIImage(named:"IMG_0398.jpg"),
+        UIImage(named:"IMG_4071.jpg"),
+        UIImage(named:"IMG_4107.jpg"),
+        UIImage(named:"IMG_4151.jpg"),
+        UIImage(named:"IMG_4194.jpg"),
+        UIImage(named:"IMG_4198.jpg"),
+        UIImage(named:"IMG_4326.jpg"),
+        UIImage(named:"IMG_4346.jpg"),
+        UIImage(named:"IMG_4358.jpg"),
+        UIImage(named:"IMG_4377.jpg"),
+        UIImage(named:"IMG_4443.jpg"),
+        UIImage(named:"IMG_4446.jpg"),
+        UIImage(named:"IMG_4470.jpg"),
+        UIImage(named:"IMG_4484.jpg"),
+        UIImage(named:"IMG_4509.jpg"),
+        UIImage(named:"IMG_4633.jpg"),
+        UIImage(named:"PomPuppySmallest.jpg"),
+        UIImage(named:"tobywings.png"),
+    ]
+
+    var steps: Int = 0
+    
+    @IBOutlet weak var motivationImage: UIImageView!
+    
+    @IBAction func swipeRight(sender: UISwipeGestureRecognizer) {
+        println("Swiped Right")
+    }
+    
+    @IBAction func swipeLeft(sender: UISwipeGestureRecognizer) {
+        println("Swiped Left")
+    }
+    
+    func randomMotivation() {
+        let idx = Int(arc4random_uniform(UInt32(imageIndex.count)))
+        dispatch_async(dispatch_get_main_queue(), {
+            self.motivationImage.image = self.imageIndex[idx]
+        })
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let start:NSDate = dateFormatter.dateFromString(dateFormatter.stringFromDate(NSDate()))!
+        
+        pedometer.startPedometerUpdatesFromDate(start, withHandler: { data, error in
+            if !(error != nil) {
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.stepCount.text = "\(data.numberOfSteps)"
+                })
+            }
+        })
+        
+        NSTimer.scheduledTimerWithTimeInterval(10, target: self, selector: Selector("randomMotivation"), userInfo: nil, repeats: true)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
